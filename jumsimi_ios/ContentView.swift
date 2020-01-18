575b840913e6b13d4c89f517febc473a7ca7e6c6
JihyeonNam-0327 <n_jihyeon@naver.com>
2020-01-18 14:00:29 +0900

    JUMSIMI iOS WKWebview 생성 완료

--- jumsimi_ios/ContentView.swift
+++ jumsimi_ios/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+//        Text("Hello, World!")
+        Webview(url: "https://www.naver.com")
     }
 }
 
